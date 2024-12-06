ea9786dacc8a9949bd9a239a7bc4d2e8d8a5f669
Grihan181 <osagrigoriy@mail.ru>
2024-12-06 15:45:02 +0400

    test commands

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import logging
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
 
 import config
 from core.handlers import users
 from core.utils import callbackdata
 from core.utils.state import StateUser
 
+commands = [
+        BotCommand(command='start', description='Старт'),
+        BotCommand(command='calculate_cost_order', description='calculate_cost_order')
+    ]
 
 async def start():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
@@ -17,8 +23,8 @@ async def start():
     dp = Dispatcher()
 
     dp.message.register(users.get_start, Command(commands='start'))
-    dp.message.register(users.mailing, Command(commands='mailing'))
-    dp.callback_query.register(users.get_start, F.data == 'start_menu')
+    #dp.message.register(users.mailing, Command(commands='mailing'))
+    #dp.callback_query.register(users.get_start, F.data == 'start_menu')
     dp.callback_query.register(users.calculate_cost_order, F.data == 'calculate_cost_order')
     dp.callback_query.register(users.select_product_category, callbackdata.StepOne.filter())
     dp.callback_query.register(users.weight_product, callbackdata.StepTwo.filter(F.category == 5))
@@ -27,6 +33,7 @@ async def start():
     dp.callback_query.register(users.specify_cost_product, callbackdata.StepThree.filter())
     dp.message.register(users.enter_price_product, StateUser.waiting_money)
 
+    await bot.set_my_commands(commands)
     await dp.start_polling(bot)
 
 
