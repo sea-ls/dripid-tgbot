fc3961bd684abacd1c789315b045f1fcfb27a8eb
Grihan181 <osagrigoriy@mail.ru>
2024-12-14 17:42:15 +0400

    add secrets

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-BOT_TOKEN = '7535051403:AAEq1VHExyZGBjFXPOCxAfnFI2oszE4rPDQ'
+import os
+
+BOT_TOKEN =  os.getenv('BOT_TOKEN')
 DB_NAME = 'base.db'
 ADMIN = [6183809792, 1308948539]
 
@@ -14,6 +16,6 @@ URL_REVIEWS = 'https://t.me/dripidfeedback'
 URL_MANAGER = 'https://t.me/dripID'
 
 # API GOOGLE
-API_KEY = 'AIzaSyDSZHrf5d-G_CCI9NQ7nkNg43KKX2BP82k'
+API_KEY = os.getenv('API_KEY')
 SERVICE = 'api/service.json'
-TABLE_ID = '1F0MAL-0sIqUYPaFKbBzYtPdM7stOyrp3H21Mbsniu2w'
+TABLE_ID = os.getenv('TABLE_ID')
